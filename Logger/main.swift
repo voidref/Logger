de1d022b5ac6cd274ad33b89540dfa9ec2bff838
Alan Westbrook <alan@rockwoodsoftware.com>
2016-07-05 14:05:12 -0700

    Initial Commit

---
+++ Logger/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Logger
+//
+//  Created by Alan Westbrook on 7/5/16.
+//  Copyright © 2016 Rockwood. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
